a9f19d4b8efb5964f52c9ec0cdc5df62cd371761
charlesdoran01 <113252036+charlesdoran01@users.noreply.github.com>
2023-02-18 13:09:15 -0500

    1

---
+++ Koi.h-DontTouchtheCubes!/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControls : MonoBehaviour
+{
+
+    [Header("Rigidbody")]
+    //Rigid body component
+    public Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //variable rb equals to component rigidbody
+        rb = GetComponent<Rigidbody>();
+        //Game us at a normal pace
+        Time.timeScale = 1f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+   
+
+}
